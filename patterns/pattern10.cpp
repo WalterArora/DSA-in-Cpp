b5851d4494d3c981ab85146f80a20581d399fe10
Aarav Arora <aarav.arora2500@gmail.com>
2025-02-01 21:32:52 +0530

    added patterns

---
+++ patterns/pattern10.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(int n){
+    for(int i = 0;i<=n;i++){
+        for(int j = 0;j<=i;j++){
+            cout<<"*";
+
+        }
+        cout<<endl;
+    }
+}
+
+void print2(int n){
+    for(int i = 0;i<=n;i++){
+        for(int j = 0;j<=n-i+1;j++){
+            cout<<"*";
+
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    print(n);
+    print2(n);
+}
